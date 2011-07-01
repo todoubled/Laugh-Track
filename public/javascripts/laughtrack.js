c984672dcdb6d1abb54c3922f8bce5af4dc49aa8
Todd Larsen <todd.larsen@designkitchen.com>
2011-07-01 15:51:20 -0500

    Added bookmarklet, updated index and laughtrack.js

--- public/javascripts/laughtrack.js
+++ public/javascripts/laughtrack.js
@@ -1,22 +1,57 @@
 LaughTrack = {
 	message:null,
-	config:null,
+	config:{
+		form:'#laugh-track',  
+		input:'#input',  
+		output:'#laughs',  
+		sendTrigger:'#send',  
+		host:'localhost',  
+		port:'10721'
+	},
 	socket:null,
+	time:new Date(),
 	clientId:Math.floor(Math.random()*11),
 	
-	init:function() {
-		LaughTrack.socket = new io.Socket(LaughTrack.config.host, {port: LaughTrack.config.port});
+	// Setup Laugh Track
+	init:function(options) {
+		LaughTrack.socket = new io.Socket(LaughTrack.config.host, { port: LaughTrack.config.port });
 		
 		// Connect client sockets and log messages to console
 		LaughTrack.socket.connect();
 		LaughTrack.socket.on('message', function(msg){
-			console.log(msg);
+			var laugh = '<p><small>'+msg.time+'</small><br />'+msg.message+'</p>';
+			$(LaughTrack.config.output).prepend(laugh);
 		});
 		
+		// Append form elements
+		LaughTrack.appendMarkup();
+		
 		// Attach click handler
-		LaughTrack.sendReady()
+		LaughTrack.sendReady();
 	},
 	
+	// Add widget to page
+	appendMarkup:function() {
+		var markup = 	'<div id="laugh-track"><form>' +
+										'<span class="directions">What do you think?</span>' +
+										'<textarea id="input"></textarea><br>' +
+										'<a id="send" href="#">Add Comment</a>' +
+										'<div id="laughs"></div>' +
+									'</form></div>';
+		var style = 	'<style type="text/css">' +
+										'span.directions { display:block; font-size:18px; }' +
+										'#laugh-track { padding:20px; z-index:9999; position:fixed; bottom:0; right:0; background:#000; opacity:0.3; width:300px; min-height:100px; max-height:300px; overflow-y:auto; }' +
+										'#laugh-track:hover { opacity:1; -webkit-transition: opacity .25s linear; transition: opacity .25s linear; }' +
+										'#laugh-track.active { opacity:1; }' +
+										'#laugh-track p { color:#fff!important; }' +
+										'#laugh-track small { font-size:0.5em; letter-spacing:1px; }' +
+									'</style>';
+									
+		$('body').append(markup);
+		$('body').append(style);
+	},
+	
+	// Add track listeners
 	sendReady:function() {
 		// Click handler to send message
 		$(LaughTrack.config.sendTrigger).bind('click', function(e) { e.preventDefault();
@@ -26,10 +61,13 @@ LaughTrack = {
 			// Construct JSON packet with message and metadata
 			LaughTrack.message = {
 				message:string,
-				from:LaughTrack.clientId
+				from:LaughTrack.clientId,
+				time:LaughTrack.time
 			};
 			
 			LaughTrack.socket.send(LaughTrack.message);
 		});
 	}
 };
+
+setTimeout(LaughTrack.init, 1000);
--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 var express = require('express');
 var app = module.exports = express.createServer();
 var sys = require('sys');
-var fs = require('fs');
 var io = require('socket.io');
 var http = require('http');
 

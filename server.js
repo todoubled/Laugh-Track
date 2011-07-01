128a26b0c74dd7bff25cc4b0e7d5211ca09e30aa
Todd Larsen <todd.larsen@designkitchen.com>
2011-07-01 00:30:19 -0500

    added jade views

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
+/*
+ * @desc Load required modules
+*/
+var express = require('express');
+var app = module.exports = express.createServer();
 var sys = require('sys');
+var fs = require('fs');
 var io = require('socket.io');
 var http = require('http');
 
-
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Laugh Track is running..');
-});
-
-server.listen(10721);
-
-
-var socket = io.listen(server);
+var socket = io.listen(app);
 
 socket.on('connection', function(client) {
   client.on('message', function(message) {
@@ -19,9 +16,34 @@ socket.on('connection', function(client) {
   }); 
 });
 
+// Config
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function() {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function() {
+	app.use(express.errorHandler()); 
+});
 
 
+// Home Page
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Laugh Track'
+  });
+});
 
-
-
-
+// Only listen if parent module
+if (!module.parent) {
+  app.listen(10721);
+  console.log("Express server listening on port %d", app.address().port);
+}
